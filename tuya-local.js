a8c94219ba73ed0efa890af38e3cfea493ab0167
subzero79 <subzero79@users.noreply.github.com>
2019-08-10 12:21:06 +1000

    Initial commit

---
+++ tuya-local.js
@@ -0,0 +1,110 @@
+const TuyaDev = require('tuyapi');
+module.exports = function(RED) {
+	
+	var interval = 15000
+
+	function getHumanTimeStamp() {
+		return new Date().toLocaleTimeString( 'en-US',{
+			hour12: false
+		});
+	}
+
+	function TuyaNode(config) {
+		RED.nodes.createNode(this,config);
+		var node = this;
+		console.log(config.devName)
+		this.Name = config.devName;
+		this.Id = config.devId;
+		this.Key = config.devKey;
+		this.Ip = config.devIp;
+		this.version = config.protocolVer;
+		const dev_info =  {name:this.Name,ip:this.Ip,id:this.Id};
+		const device = new TuyaDev({
+			id: this.Id,
+			key: this.Key,
+			ip: this.Ip,
+			version: this.version});
+
+		function connectToDevice(timeout,req) {
+			device.find({'options': {'timeout':timeout}}).then( () => {
+				node.status({fill:"yellow",shape:"dot",text:"connecting"});
+				node.log(req);
+				device.connect().then( () => {
+				}, (reason) => { 
+					node.status({fill:"red",shape:"ring",text:"failed: " + reason});
+				});
+			});
+		}
+// 
+		function setDevice(req) {
+			if ( req == "request" ) {
+				device.get({"schema":true});
+			} else if ( req == "connect" ) {
+				// node.log('Connection requested by input');
+				connectToDevice(10,'Connection requested by input');
+			} else if ( req == "disconnect" ) {
+				node.log("Disconnection requested by input")
+				device.disconnect();
+			} else if (req == "toggle") {
+				device.toggle();
+			} else if ( typeof req == "boolean" ) {
+				device.set({set: req}).then( () => {
+					node.status({fill:"green",shape:"dot",text: 'set success at:' + getHumanTimeStamp()});
+				}, (reason) => {
+					node.status({fill:"red",shape:"dot",text: 'set state failed:' + reason});
+				});	
+			} else if ( "dps" in req ) {
+				console.log(req)
+				device.set(req);
+			} else if ( "multiple" in req) {
+				device.set({
+					multiple:true,
+					data: req.data
+				});
+			}
+		}
+
+		connectToDevice(10,'Deploy connection request');
+
+		device.on('disconnected', () => {
+			this.status({fill:"red",shape:"ring",text:"disconnected from device"});
+			dev_info.available = false
+			msg = {data:dev_info}
+			node.send(msg);
+			timeout = setTimeout(connectToDevice, 10000, device, 10, node,'set timeout for re-connect');
+		});
+
+
+		device.on('connected', () => {
+			this.status({fill:"green",shape:"dot",text: this.Ip + " at " + getHumanTimeStamp()});
+			clearTimeout(timeout)
+		});
+
+		device.on('error', error => {
+			this.status({fill:"red",shape:"ring",text:"error: " + error});
+			node.warn('error ' + error);
+		});
+
+		device.on('data', (data,commandByte) => {
+			if ("commandByte" !== null ) {
+				dev_info.available = true
+				msg = {data:dev_info,commandByte:commandByte,payload:data};
+				node.send(msg);
+			}
+		});
+
+		node.on('input', function(msg) {
+			setDevice(msg.payload);
+		});
+
+		this.on('close', function() {
+			if (device.isConnected()) {
+				device.disconnect();
+			}
+		});
+
+
+	}
+	RED.nodes.registerType("tuya-local",TuyaNode);
+}
+
